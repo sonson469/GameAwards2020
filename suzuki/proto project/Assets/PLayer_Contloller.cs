756ff14ce90c733c244513fa905766d17a08ad6d
popo1050 <szkait211@gmail.com>
2020-02-17 17:15:17 +0900

    proto type

---
+++ suzuki/proto project/Assets/PLayer_Contloller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayer_Contloller : MonoBehaviour
+{
+    public float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("right"))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+
+
+    }
+}
