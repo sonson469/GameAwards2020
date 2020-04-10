b9630611c1f1eb4fe602243a7026438033eaed9f
sonson469 <sonson.owata@icloud.com>
2020-04-11 01:26:43 +0900

    更新内容はテキスト見てね

--- 李/FROGIL/FROGIL prototype/Assets/Enemy.cs
+++ 李/FROGIL/FROGIL prototype/Assets/Enemy.cs
@@ -9,6 +9,7 @@ public class Enemy : MonoBehaviour
 {
 
     public GameObject target;
+    public string tagname;
     private NavMeshAgent agent;
 
     // Start is called before the first frame update
@@ -20,8 +21,24 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         //ターゲットの位置を目的地に設定する
         agent.destination = target.transform.position;
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == tagname)
+        {
+            agent.speed = 0.0f;
+            agent.angularSpeed = 0.0f;
+            Invoke("Release", 8.0f);
+        }
+    }
+
+    void Release()
+    {
+        agent.speed = 0.5f;
+        agent.angularSpeed = 120.0f;
     }
 }
