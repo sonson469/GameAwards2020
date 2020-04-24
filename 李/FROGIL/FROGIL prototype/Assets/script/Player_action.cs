6360d11f2bafa22cfd0394103cdab4ed9158fa6a
sonson469 <sonson.owata@icloud.com>
2020-04-24 21:53:15 +0900

    油メーター移してきた

--- 李/FROGIL/FROGIL prototype/Assets/script/Player_action.cs
+++ 李/FROGIL/FROGIL prototype/Assets/script/Player_action.cs
@@ -12,25 +12,32 @@ public class Player_action : MonoBehaviour
     public float offsetX = 0f;
     public float offsetY = 1f;
     public float offsetZ = 0.5f;
-
-    
+    //クールタイム
+    public bool Use;
+    public float cooltime;
+    public float oilmator;
 
     bool pushflag = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Use = true;
+        cooltime = 0.0f;
+        oilmator = 8.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //8回使うと５秒間使えなくなる
         if(Input.GetKey(PushKey))
         {
-            if(pushflag == false)
+            if(pushflag == false && Use == true && oilmator > 0.0f)
             {
                 pushflag = true;
+                //Use = false;
+                oilmator -= 1.0f;
 
                 Vector3 newpos = this.transform.position;
 
@@ -48,12 +55,39 @@ public class Player_action : MonoBehaviour
                 throwV = this.transform.rotation * throwV;
                 //rbody.AddForce(throwV, ForceMode.Impulse);
 
+                GameObject director = GameObject.Find("GameDirector");
+                director.GetComponent<UI>().DecreseOil();
+                
+
             }
+            
+
         }
         else
         {
             //押す解除
             pushflag = false;
+
         }
+
+        if (oilmator <= 0.0f)
+        {
+            Use = false;
+
+
+
+            if (Use == false)
+            {
+                cooltime += Time.deltaTime;
+
+            }
+            if (cooltime >= 5.0f)
+            {
+                Use = true;
+                cooltime = 0.0f;
+                oilmator = 8.0f;
+            }
+        }
+        
     }
 }
--- 李/FROGIL/FROGIL prototype/Assets/script/UI.cs
+++ 李/FROGIL/FROGIL prototype/Assets/script/UI.cs
@@ -6,44 +6,46 @@ using UnityEngine.UI;
 public class UI : MonoBehaviour
 {
     //時間制限処理
-    public GameObject Timertext;
-    float time = 90.0f;
+    GameObject Timertext;
+    //float time = 90.0f;
     //オイル処理
-    public GameObject oilGauge;
-    float Oil = 1;
+    GameObject oilGauge;
+    
+
+    public GameObject Player_action;
+    Player_action script;
+    public float oil;
     // Start is called before the first frame update
     void Start()
     {
         //タイムオブジェクト見つける
-        Time.timeScale = 1;
-        this.Timertext = GameObject.Find("Time");
+       // Time.timeScale = 1;
+       // this.Timertext = GameObject.Find("Time");
         //オイルオブジェクト見つける
         this.oilGauge = GameObject.Find("oilGauge");
+        Player_action = GameObject.Find("Player");
+        script = Player_action.GetComponent<Player_action>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.time -= Time.deltaTime;
-        this.Timertext.GetComponent<Text>().text = this.time.ToString("F1");
+        oil = script.oilmator;
+        //Debug.Log(oil);
+       // this.time -= Time.deltaTime;
+        //this.Timertext.GetComponent<Text>().text = this.time.ToString("F1");
         //時間切れ、処理止める
-        if(time <= 0)
-        {
+        //if(time <= 0)
+        //{
             //this.time = 0;
             //Time.timeScale = 0;
-        }
-        if(Oil >1)
+        //}
+
+       
+        if (oil <= 0)
         {
-            Oil = 1;
-        }
-        if(Oil < 0)
-        {
-            Oil = 0;
-        }
-        if (Oil <= 0)
-        {
-            this.oilGauge.GetComponent<Image>().fillAmount +=0.005f;
-            Oil += 0.005f;
+             this.oilGauge.GetComponent<Image>().fillAmount +=0.2f * Time.deltaTime;
+              
         }
 
 
@@ -52,12 +54,13 @@ public class UI : MonoBehaviour
     public void DecreseOil()
     {
         
-            if (Oil > 0)
+            if (oil > 0)
             {
-                this.oilGauge.GetComponent<Image>().fillAmount -= 0.25f;
-                Oil -= 0.25f;
+                this.oilGauge.GetComponent<Image>().fillAmount -= 0.125f;
+                
             }
             
+
             
         
         
