fdf508e7e7336bdf3bf1e476f30204ac20b65895
sonson469 <sonson.owata@icloud.com>
2020-04-27 01:23:09 +0900

    カメラ移動

--- 李/FROGIL/FROGIL prototype/Assets/script/MainCamera.cs
+++ 李/FROGIL/FROGIL prototype/Assets/script/MainCamera.cs
@@ -5,22 +5,35 @@ using UnityEngine;
 public class MainCamera : MonoBehaviour
 {
 
-    public float speed;   //速度
-    public float radius;  //半径
+    public float speed = 40;   //速度
+    public float radius = 30;  //回転半径
 
     public GameObject center;  //視点
 
-    float posx;
-    float posz;
+    private Vector3 pos;
 
     //キー
-    public string PushKeyRight;
-    public string PushKeyLeft;
+    public string PushKeyRight = "j";
+    public string PushKeyLeft = "l";
+    public string PushKeyUp = "i";
+    public string PushKeyDown = "k";
+
+    //カメラ座標取得
+    private Vector3 CameraPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*pos.x = radius * Mathf.Sin(-speed * Time.time) - 10.0f;
+        pos.z = radius * Mathf.Cos(-speed * Time.time) + 12.0f;
+        pos.z -= radius * 2;
+        pos.y = 8.0f;
+        this.gameObject.transform.position = new Vector3(pos.x, pos.y, pos.z);
+        this.gameObject.transform.LookAt(center.transform);*/
+
+        Vector3 axis = transform.TransformDirection(Vector3.down);
+        transform.RotateAround(center.transform.position, axis, speed * Time.deltaTime);
+        this.gameObject.transform.LookAt(center.transform);
     }
 
     // Update is called once per frame
@@ -29,18 +42,26 @@ public class MainCamera : MonoBehaviour
 
         if (Input.GetKey(PushKeyRight))
         {
-            posx = radius * Mathf.Sin(Time.time * speed);
-            posz = radius * Mathf.Cos(Time.time * speed);
-
-            this.gameObject.transform.position = new Vector3(posx, 0, posz);
+            Vector3 axis = transform.TransformDirection(Vector3.down);
+            transform.RotateAround(center.transform.position, axis, speed * Time.deltaTime);
             this.gameObject.transform.LookAt(center.transform);
         }
         if (Input.GetKey(PushKeyLeft))
         {
-            posx = radius * Mathf.Sin(Time.time * -speed);
-            posz = radius * Mathf.Cos(Time.time * -speed);
-
-            this.gameObject.transform.position = new Vector3(posx, 0, posz);
+            Vector3 axis = transform.TransformDirection(Vector3.up);
+            transform.RotateAround(center.transform.position, axis, speed * Time.deltaTime);
+            this.gameObject.transform.LookAt(center.transform);
+        }
+        if (Input.GetKey(PushKeyUp))
+        {
+            Vector3 axis = transform.TransformDirection(Vector3.left);
+            transform.RotateAround(center.transform.position, axis, speed * Time.deltaTime);
+            this.gameObject.transform.LookAt(center.transform);
+        }
+        if (Input.GetKey(PushKeyDown))
+        {
+            Vector3 axis = transform.TransformDirection(Vector3.right);
+            transform.RotateAround(center.transform.position, axis, speed * Time.deltaTime);
             this.gameObject.transform.LookAt(center.transform);
         }
 
