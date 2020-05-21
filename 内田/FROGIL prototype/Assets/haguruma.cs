ad5608127c9fdba8dda35c5af1836d44fbad693d
kito0207 <60766301+kito0207@users.noreply.github.com>
2020-05-22 02:17:04 +0900

    zyouge

--- 内田/FROGIL prototype/Assets/haguruma.cs
+++ 内田/FROGIL prototype/Assets/haguruma.cs
@@ -17,6 +17,7 @@ public class haguruma : MonoBehaviour
 
     public bool oilflag = false;          //油がかかったか
     public bool gearflag = false;         //回転
+    public Vector3 stageuppos;              //ステージアップの座標を入れる場所
 
 
     //********************
@@ -28,6 +29,7 @@ public class haguruma : MonoBehaviour
     void Start()
     {
         rbody = this.GetComponent<Rigidbody>();
+        stageuppos = GameObject.Find("stageup").transform.position;//上下するやつにstageuoの座標を入れる
     }
 
     // Update is called once per frame
@@ -53,6 +55,7 @@ public class haguruma : MonoBehaviour
             if (gearflag == true && collider.gameObject.tag == Tongue)//trueかつ舌と当たったら
             {
                 transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime);//毎秒90度回転する
+                rbody.constraints = RigidbodyConstraints.None;
                 Invoke("stageup", 4);
             }
         }
@@ -61,15 +64,16 @@ public class haguruma : MonoBehaviour
     void stageup()
     {
         gearflag = false;
-        Vector3 newpos = GameObject.Find("stageup").transform.position;//newposにstageuoの座標を入れる
-        GameObject.Find("stageup").transform.position += new Vector3(0f, 2.0f * Time.deltaTime, 0f);//毎秒Y軸に2増加
-        Invoke("up", 2);
+        //Vector3 newpos= GameObject.Find("stageup").transform.position;//newposにstageuoの座標を入れる
+        //GameObject.Find("stageup").transform.position += Vector3 newpos(0f, 2.0f * Time.deltaTime, 0f);//毎秒Y軸に2増加
+        stageuppos = new Vector3 (stageuppos.x, stageuppos.y +Mathf.PingPong(Time.time,2.0f),stageuppos.z);
+        //Invoke("up", 2);
     }
-
+    /*
     void up()
     {
 
         rbody.constraints = RigidbodyConstraints.FreezeAll;
     }
-
+    */
 }
