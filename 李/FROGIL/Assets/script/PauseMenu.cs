44e7873815a64d310e4e7f9f9584e34f24393268
sonson469 <sonson.owata@icloud.com>
2020-06-14 17:23:23 +0900

    完成に近い

---
+++ 李/FROGIL/Assets/script/PauseMenu.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject Pause;
+
+    public GrassClear Clear;
+
+    public AudioSource AudioSource;
+
+    public AudioClip pause;
+    public AudioClip retry;
+    public AudioClip retire;
+
+
+    public bool PauseFlag = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pause.SetActive(false);
+        AudioSource = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Menu") && !Clear.ClearFlag)
+        {
+            AudioSource.PlayOneShot(pause);
+            Pause.SetActive(!Pause.activeSelf);
+        }
+        if (Pause.activeSelf)
+        {
+            PauseFlag = true;
+            Time.timeScale = 0f;
+        }
+        if(!Pause.activeSelf && !Clear.ClearFlag)
+        {
+            PauseFlag = false;
+            Time.timeScale = 1f;
+        }
+
+    }
+
+    public void Keep()
+    {
+        AudioSource.PlayOneShot(retry);
+        Pause.SetActive(false);
+    }
+
+    public void Retry()
+    {
+        AudioSource.PlayOneShot(retry);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Retire()
+    {
+        AudioSource.PlayOneShot(retire);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Gameover");
+    }
+}
---
+++ 李/FROGIL/Assets/script/SwitchMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMove : MonoBehaviour
+{
+
+    public string PushTag;
+    public GameObject MoveField;  //動くやつ
+
+    //public rbodyScript targetrbody;
+
+    public float speedx = 0f;
+    public float speedy = 0f;
+    public float speedz = 0f;
+
+    public float Plus;
+    public float Minus;
+
+    public bool moveflag = false;
+    //public bool plusflag = false;        //プラスの上限にいるか
+    //public bool minusflag = false;       //マイナスの上限にいるか
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (moveflag==true)
+        {
+            MoveField.transform.Translate(-speedx * Time.deltaTime, -speedy * Time.deltaTime, -speedz * Time.deltaTime);
+        }
+        if ( MoveField.transform.position.x < Minus)
+        {
+            moveflag = false;
+        }
+    }
+
+    //**********************
+    // 箱と重なったら
+    //**********************
+    private void OnTriggerEnter(Collider collider)
+    {
+        if ( collider.gameObject.tag == PushTag)
+        {
+            moveflag = true;
+        }
+    }
+    
+}
+
